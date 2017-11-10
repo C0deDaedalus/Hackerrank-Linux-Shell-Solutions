a58a67fef858a7a90bef5375426dc402d142eb03
Amit Kumar <31804724+C0deDaedalus@users.noreply.github.com>
2017-11-10 05:45:25 +0530

    Update 3.A_Personalized_Echo.sh

--- 3.A_Personalized_Echo.sh
+++ 3.A_Personalized_Echo.sh
@@ -17,3 +17,28 @@ echo
 echo -n "Enter your Full name : "   # -n means don't go to next line.
 read fullname                       # Take user input & store in variable fullname
 echo "Hi $fullname"                 # echo Hi fullname
+
+# __________________________________________________________________ Asking for User login Details
+echo
+read -p 'Username : ' user              # -p means prompt for input
+read -sp 'Password : ' pass             # Supress the user input display. Passwords are meant to be displayed.
+echo
+echo "Logged in as $user with $pass "
+echo
+
+
+# __________________________________________________________________ Reading Multiple User Inputs into variables
+echo
+echo "Enter any 3 OS Prefrences in order with most likely to choose first : "
+echo -ne "\t\t\t"
+read os1 os2 os3
+echo "You like $os1 most. Me too"
+echo
+
+# When we supply several variable names to read, It takes our input as a whole one input,
+# & then splits it on whitespaces
+# The first item then becomes first user input & gets stored in first variable.
+# It goes on till the last variable
+# If there are more items than variable names then remaining items will all be added to the last variable name.
+# If there are less items than variable names then remaining variable names will be set to blank or null.
+
